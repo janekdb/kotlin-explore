232a81e1ae3e0d720702f4509f88811d35c42fc9
Janek Bogucki <janekdb@gmail.com>
2021-11-04 22:58:01 +0000

    External pattern definitions

--- src/main/kotlin/life/Library.kt
+++ src/main/kotlin/life/Library.kt
@@ -1,64 +1,24 @@
 package life
 
+import java.lang.IllegalStateException
+
 data class Offset(val x: Int, val y: Int)
 
 object Library {
 
-    val GLIDER = """
-        .X.
-        ..X
-        XXX
-    """.trimIndent()
+    val GLIDER = "glider"
 
     // https://en.wikipedia.org/wiki/Gun_(cellular_automaton)
-    val GOSPER_GLIDER_GUN = """
-        ........................X...........
-        ......................X.X...........
-        ............XX......XX............XX
-        ...........X...X....XX............XX
-        XX........X.....X...XX..............
-        XX........X...X.XX....X.X...........
-        ..........X.....X.......X...........
-        ...........X...X....................
-        ............XX......................
-    """.trimIndent()
+    val GOSPER_GLIDER_GUN = "gosper-glider-gun"
 
     //https://conwaylife.com/wiki/Copperhead
-    val COPPERHEAD = """
-        .XX..XX.
-        ...XX...
-        ...XX...
-        X.X..X.X
-        X......X
-        ........
-        X......X
-        .XX..XX.
-        ..XXXX..
-        ........
-        ...XX...
-        ...XX...
-    """.trimIndent()
+    val COPPERHEAD = "copperhead"
 
     // https://conwaylife.com/wiki/Noah%27s_ark
-    val NOAHS_ARK = """
-        ..........X.X..
-        .........X.....
-        ..........X..X.
-        ............XXX
-        ...............
-        ...............
-        ...............
-        ...............
-        ...............
-        .X.............
-        X.X............
-        ...............
-        X..X...........
-        ..XX...........
-        ...X...........
-    """.trimIndent()
-    fun offsets(definition: String): Set<Offset> {
-        val rows = definition.lines()
+    val NOAHS_ARK = "noahs-ark"
+
+    fun offsets(name: String): Set<Offset> {
+        val rows = loadDefinition(name)
         val result = rows.mapIndexed { rowIndex, row ->
             row.mapIndexed { colIndex, char ->
                 Pair(colIndex, char)
@@ -70,5 +30,11 @@ object Library {
             }
         }
         return result.flatten().toSet()
+    }
+
+    private fun loadDefinition(name: String): List<String> {
+        val definitionText = javaClass.getResource("/definition/$name.def")?.readText()
+            ?: throw IllegalStateException("Definition missing: $name.def")
+        return definitionText.lines()
     }
 }
--- src/test/kotlin/life/LibraryTest.kt
+++ src/test/kotlin/life/LibraryTest.kt
@@ -8,6 +8,13 @@ internal class LibraryTest {
     private fun o(x: Int, y: Int) = Offset(x, y)
 
     @Test
+    fun definitionLoaded() {
+        val offsets = Library.offsets("test-definition")
+        val expected = setOf(o(1, 0), o(2, 1), o(0, 2), o(1, 2), o(2, 2))
+        assertEquals(expected, offsets)
+    }
+
+    @Test
     fun gliderOffsetsAreProvided() {
         val offsets = Library.offsets(Library.GLIDER)
         val expected = setOf(o(1, 0), o(2, 1), o(0, 2), o(1, 2), o(2, 2))
