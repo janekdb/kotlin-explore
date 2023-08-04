3ea8c086b3984ea80a0383ade150c5f85c049fdc
Janek Bogucki <janekdb@gmail.com>
2023-08-04 23:19:18 +0100

    Upgrade test framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
     testImplementation(kotlin("test"))
 }
 
